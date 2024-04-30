e7c110f1cf170f09b8ee5331232391cb92626418
0YKahil <kahilyoussef12@gmail.com>
2024-04-30 15:43:14 -0700

    implemented the rowReduce function

--- matrix.c
+++ matrix.c
@@ -8,7 +8,6 @@
 #define MAX_ROWS 7
 #define MAX_COLS 7
 
-// parse string matrix
 
 /*
  * Takes a m x n matrix inputted by the user in the form {{a11, a12, a13}, {a21, a22, a23}, {a31, a32, a33}}
@@ -19,12 +18,12 @@
 
 void parse_matrix() {}
 
-// Row Reduction RREF
 
 /*
- *  Takes an row x columns and prints it in the form:   a11 a12 a13
- *                                                      a21 a22 a23
- *                                                      a31 a32 a33
+ * Takes a row x columns matrix 
+ * and prints it in the form:   a11 a12 a13
+ *                              a21 a22 a23
+ *                              a31 a32 a33
  */
 void printmatrix(int rows, int cols, float matrix[rows][cols]) {
     for (int i = 0; i < rows; i++) {
@@ -36,10 +35,44 @@ void printmatrix(int rows, int cols, float matrix[rows][cols]) {
     printf("\n");
 }
 
+
+
+// Row Reduction RREF
+/*
+ * Takes a row x columns matrix and row reduces it to Reduced Row Echelon Form
+ */
+void rowReduce(int rows, int cols, float matrix[rows][cols]) {
+    const int nrows = rows;
+    const int ncols = cols;
+    int lead = 0;
+
+    while (lead < nrows) {
+        float d, m;
+
+        for (int r = 0; r < nrows; r++) {
+            // calculate divisor d and multiplier m
+            d = matrix[lead][lead];
+            m = matrix[r][lead] / matrix[lead][lead];
+
+            for (int c = 0; c < ncols; c++) {
+                if (r == lead) {
+                    matrix[r][c] /= d;
+                } else {
+                    matrix[r][c] -= matrix[lead][c] * m;
+                }
+            }
+        }
+        lead++;
+        printmatrix(rows, cols, matrix);
+    }
+}
+
 int main() {
     float A[3][4] = {{5, -6, -7,   7},
                      {3, -2,  5, -17},
                      {2,  4, -3,  29}}; //answer should be {2, 4, -3}
 
-    printmatrix(3 , 4, A);
+    printmatrix(3, 4, A);
+    rowReduce(3, 4, A);
+
 }
