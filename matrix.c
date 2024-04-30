969bc5273a5dbd0205433dc469f977f74c7c90d3
0YKahil <kahilyoussef12@gmail.com>
2024-04-30 15:34:12 -0700

    Implemented printmatrix function

--- matrix.c
+++ matrix.c
@@ -2,31 +2,44 @@
 #include <string.h>
 #include <stdlib.h>
 
-// Place holders for now
-#define ROWS 3
-#define COLS 4
+/* Maximum will be capped at 7 as going higher than that would 
+ * exponentially increase the time needed to calculate
+ */
+#define MAX_ROWS 7
+#define MAX_COLS 7
 
-int main() {
-    printf("Hello World\n");
+// parse string matrix
 
-    double matrix[ROWS][COLS] = {
-        {1, 2, -1, 0},
-        {2, 3, 1, 10},
-        {3, 5, 0, 15}
-    };
+/*
+ * Takes a m x n matrix inputted by the user in the form {{a11, a12, a13}, {a21, a22, a23}, {a31, a32, a33}}
+ * representing the matrix:  a11 a12 a13
+ *                           a21 a22 a23
+ *                           a31 a32 a33
+ */
 
-
-    return 0;
-}
-
+void parse_matrix() {}
 
 // Row Reduction RREF
 
-// Linear Dependence/Independence
+/*
+ *  Takes an row x columns and prints it in the form:   a11 a12 a13
+ *                                                      a21 a22 a23
+ *                                                      a31 a32 a33
+ */
+void printmatrix(int rows, int cols, float matrix[rows][cols]) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%7.4f  ", matrix[i][j]);
+        }
+        printf("\n"); // new line after each row
+    }
+    printf("\n");
+}
 
-// Matrix Multiplication
+int main() {
+    float A[3][4] = {{5, -6, -7,   7},
+                     {3, -2,  5, -17},
+                     {2,  4, -3,  29}}; //answer should be {2, 4, -3}
 
-// Null Spaces and Bases
-
-// Determinant
-
+    printmatrix(3 , 4, A);
+}
