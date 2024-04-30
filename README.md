69758e73ba9c606b0cc8311e6bb35572349553e2
0YKahil <kahilyoussef12@gmail.com>
2024-04-30 13:44:42 -0700

    Set up basic matrix example and place holders

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Matrix-Operations-Calculator
-Matrix calculator for doing Matrix operations such as Row Reduction, RREF, Finding Null Spaces and Ranks, as well as matrix multiplications and additions.
+Matrix calculator for doing Matrix operations such as Row Reduction, RREF, Finding Null Spaces and Ranks, determinants, as well as matrix multiplications and additions.
---
+++ matrix.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Place holders for now
+#define ROWS 3
+#define COLS 4
+
+int main() {
+    printf("Hello World\n");
+
+    double matrix[ROWS][COLS] = {
+        {1, 2, -1, 0},
+        {2, 3, 1, 10},
+        {3, 5, 0, 15}
+    };
+
+
+    return 0;
+}
+
+
+// Row Reduction RREF
+
+// Linear Dependence/Independence
+
+// Matrix Multiplication
+
+// Null Spaces and Bases
+
+// Determinant
+
